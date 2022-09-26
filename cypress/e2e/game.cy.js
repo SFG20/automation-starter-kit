19e0dff75ef65319b7a1bd30409667b53e48bf6e
JosephLee7123 <114394141+JosephLee7123@users.noreply.github.com>
2022-09-26 16:19:18 +0100

    Learning Auto Test

--- cypress/e2e/game.cy.js
+++ cypress/e2e/game.cy.js
@@ -1,12 +1,18 @@
 describe('Turn based game', () => {
     before(() => {
-        //cy.visit('https://sfg20.github.io/automation-starter-kit/') // Uncomment this line to visit the remote hosted site if you're not running it locally.
-        cy.visit('http://localhost:3000') // If you are using a local server, you can use this line instead of the line above.
+        cy.visit('https://sfg20.github.io/automation-starter-kit/') // Uncomment this line to visit the remote hosted site if you're not running it locally.
+        //cy.visit('http://localhost:3000') // If you are using a local server, you can use this line instead of the line above.
          
     })
     it('Example: Places an X in the top left square', () => {
         cy.get('.cell1').click()
         cy.contains('.cell1', 'X')
         cy.contains('[data-testid=message]', 'Next turn is 0')
+    })
+
+    it('Example: Places an 0 in the Middle Square', () => {
+        cy.get('.cell5').click()
+        cy.contains('.cell5', '0')
+        cy.contains('[data-testid=message]', 'Next turn is X')
     })
 })
