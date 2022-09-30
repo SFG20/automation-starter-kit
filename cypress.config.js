cca777140e0247f545f6e95a9776111022afc46a
Andy Davies <globalroo@hotmail.com>
2022-09-30 18:40:24 +0100

    Add cookie and localstore persistance

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,39 @@
 const { defineConfig } = require('cypress')
+const fs = require('fs')
 
-module.exports = defineConfig({
-  video: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-  },
-})
+// Get environment variables from .git for deploy. Github actions have replaced it.
+// const [__, branch] = fs.readFileSync("../.git/HEAD", "utf-8").trim().split("refs/heads/")
+// const branchURL = branch.split("/").join("-")
+
+const users = require('./cypress/fixtures/users.json')
+const servers = require('./cypress/fixtures/servers.json')
+console.log(servers)
+module.exports = {
+    default: defineConfig({
+        projectId: 'evt7b4',
+        e2e: {
+            defaultCommandTimeout: 10000,
+            env: {
+                host: 'localhost',
+                port: '3030',
+                protocol: 'http',
+                users,
+                servers,
+            },
+            setupNodeEvents(on, config) {
+                // implement node event listeners here
+                on('task', {
+                    log({ log, type = 'log' }) {
+                        if (typeof log === 'object') {
+                            log = JSON.stringify(log, null, 2)
+                        }
+                        console[type](log)
+                        return null
+                    },
+                })
+            },
+        },
+    }),
+}
+
+// npx cypress run --record --key 7f1c44fb-fa55-4a12-a518-f33ded694d04
