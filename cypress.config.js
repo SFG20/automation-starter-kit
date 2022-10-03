3e9b184128ac2630ef599f975a8b29f915aec821
JosephLee7123 <114394141+JosephLee7123@users.noreply.github.com>
2022-10-03 14:57:42 +0100

    Merged Changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,6 @@
 const { defineConfig } = require('cypress')
 const fs = require('fs')
 
-<<<<<<< HEAD
-module.exports = defineConfig({
-  video: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-  },
-
-})
-
-{ chromeWebSecurity: true }
-
-=======
 // Get environment variables from .git for deploy. Github actions have replaced it.
 // const [__, branch] = fs.readFileSync("../.git/HEAD", "utf-8").trim().split("refs/heads/")
 // const branchURL = branch.split("/").join("-")
@@ -53,4 +37,3 @@ module.exports = {
 }
 
 // npx cypress run --record --key 7f1c44fb-fa55-4a12-a518-f33ded694d04
->>>>>>> 6048b935e82fae3ac61b4cb1fb9be7218c0bd845
